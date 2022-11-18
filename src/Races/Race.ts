bd145504483c83cb51afad52df516bfae16b8672
Erik Yamamoto <erik.ymmt@gmail.com>
2022-11-18 16:12:59 -0300

    fix: Fix lint - single quote

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -2,7 +2,7 @@ export default abstract class Race {
   constructor(private _name: string, private _dexterity: number) {}
 
   static createdRacesInstances(): number {
-    throw new Error("Not implemented");
+    throw new Error('Not implemented');
   }
 
   abstract get maxLifePoints(): number;
